ca302163cec3f9ef702dc92e247fe4953e101793
wieandk <wieandk@chop.edu>
2024-01-09 10:22:46 -0500

    updates to output

--- code/cohort_scv_fot.R
+++ code/cohort_scv_fot.R
@@ -1,86 +1,4 @@
 
-
-#' HR COMMENTS: To discuss with Charlie --- is there a way 
-#' to take out the DQ specific function from this time function 
-#' so that the time function can be more reusable across 
-#' different check types?
-
-compute_fot_scv <- function(cohort,
-                            code_type,
-                            concept_set,
-                            code_domain,
-                            time_period='year',
-                            time_span= c('2012-01-01','2022-12-31'),
-                            site_list=list('stanford',
-                                           'colorado',
-                                           'chop'),
-                           domain_tbl=read_codeset('scv_domains','cccc')
-) {
-  
-  if(!is.vector(concept_set)){stop('For an over time output, please select 1-5 codes from your
-                                   concept set and include them as a vector in the concept_set argument.')}
-  if(is.vector(concept_set) && length(concept_set) > 5){stop('For an over time output, please select 1-5 
-                                                              codes from your concept set and include them as
-                                                             a vector in the concept_set argument.')}
-  
-  site_list_v <- unlist(site_list)
-  
-  final_results <- list()
-  
-  t1 <- seq(from=ymd(time_span[[1]]),to=ymd(time_span[[2]]),by=time_period)
-  t2 <- ceiling_date(t1, time_period) - 1
-  
-  
-  # narrows the visit time to cohort_entry and end date
-  for(k in 1:length(t1)) {
-    
-    message(paste0('Starting ',t1[[k]]))
-    
-    target <- ymd(t1[[k]])
-    
-    baseline_start_date <- target
-    baseline_end_date <- ceiling_date(target, time_period) - 1
-    
-    cohort_narrowed <- cohort %>% 
-      mutate(start_date = as_date(baseline_start_date),
-             end_date = as_date(baseline_end_date))
-    
-    
-    cohort_narrow_prepped <- cohort_narrowed %>%
-      filter(site %in% site_list_v) %>% mutate(time_period=start_date,
-                                               time_increment=time_period)
-    
-    concept_set_prep <- as.data.frame(concept_set) %>% rename('concept_id' = concept_set) %>%
-      mutate(concept_id = as.integer(concept_id))
-    concept_set_prep <- copy_to_new(df = concept_set_prep)
-    
-    scv <- check_code_dist(cohort = cohort_narrow_prepped,
-                           concept_set = concept_set_prep,
-                           code_type = code_type,
-                           code_domain = code_domain,
-                           time = TRUE) %>% mutate(start_date = target,
-                                                   end_date = baseline_end_date,
-                                                   time_increment=time_period)
-    
-    # scv_reduced <- dplyr::bind_rows(scv, .id='visit_type') %>% 
-    #   mutate(time_increment=time_period)
-    
-    final_results[[k]] <- scv
-    
-  }
-  
-  # final_results[[paste0(check_string,'_meta')]] <- meta
-  output = reduce(.x=final_results,
-                  .f=dplyr::union)
-  
-  return(output)
-  
-}
-
-
-
-
-
 #' OUTPUT GENERATION
 
 #' HR COMMENT: The parameter to this function `scv_process` is
@@ -99,28 +17,55 @@ compute_fot_scv <- function(cohort,
 #' name is? Also, what is the anomaly here? I think this is exploratory, correct?
 #' Should we create a caption that explains the content of this graph? 
 #' 
-scv_ss_exp_at <- function(scv_process_tbl,
+scv_ss_ms_exp_at <- function(process_output,
                           output,
-                          facet){
+                          facet,
+                          vocab_tbl = vocabulary_tbl('concept')){
   
-  if(output == 'source_prop'){
+  if(output == 'source'){
     facet <- facet %>% append('source_concept_id')
     color <- 'concept_id'
-  }else if(output == 'concept_prop'){
+    prop <- 'source_prop'
+  }else if(output == 'cdm'){
     facet <- facet %>% append('concept_id')
     color <- 'source_concept_id'
-  }else{stop('Please select a valid output')}
+    prop <- 'concept_prop'
+  }else{stop('Please select a valid output - `source` or `cdm`')}
   
-  p <- scv_process_tbl %>%
-    mutate(concept_id = as.character(concept_id),
-           source_concept_id = as.character(source_concept_id)) %>%
-    ggplot(aes(y = !!sym(output), x = start_date, color = !!sym(color))) +
-    geom_line() +
-    facet_wrap((facet)) +
-    labs(title = 'Code Mapping Pairs Over Time')
   
-  #' HR COMMENT: Need to add plotly as a library to call
-  ggplotly(p)
+  if(is.null(vocab_tbl)){
+    
+    p <- process_output %>%
+      mutate(concept_id = as.character(concept_id),
+             source_concept_id = as.character(source_concept_id)) %>%
+      ggplot(aes(y = !!sym(prop), x = time_start, color = !!sym(color))) +
+      geom_line() +
+      facet_wrap((facet)) +
+      labs(title = 'Code Mapping Pairs Over Time')
+
+    ggplotly(p)
+    
+  }else{
+    
+    process_output_db <- copy_to_new(df = process_output)
+    
+    process_output_plot <- process_output_db %>%
+      mutate(join_col = !!sym(color)) %>%
+      left_join(select(vocab_tbl, concept_id, concept_name), 
+                by = c('join_col' = 'concept_id'))
+    
+    p <- process_output_plot %>%
+      mutate(concept_id = as.character(concept_id),
+             source_concept_id = as.character(source_concept_id)) %>%
+      ggplot(aes(y = !!sym(prop), x = time_start, color = !!sym(color),
+                 text = paste('concept_name: ', concept_name))) +
+      geom_line() +
+      facet_wrap((facet)) +
+      labs(title = 'Code Mapping Pairs Over Time',
+           color = color)
+    
+    ggplotly(p)
+  }
   
 }
 
@@ -198,34 +143,36 @@ produce_multisite_mad_scv <- function(multisite_tbl,
   
 }
 
-scv_ms_anom_at <- function(scv_process,
+scv_ms_anom_at <- function(process_output,
                            output,
                            facet,
                            mad_dev = 2){
   
   facet <- facet %>% append(c('concept_id', 'source_concept_id'))
   
-  if(output == 'source_prop'){
+  if(output == 'source'){
     y_col <- 'source_concept_id'
-    code_type <- 'source'
-  }else if(output == 'concept_prop'){
+    prop <- 'source_prop'
+  }else if(output == 'cdm'){
     y_col <- 'concept_id'
-    code_type <- 'cdm'
-  }else{stop('Please select a valid output')}
+    prop <- 'concept_prop'
+  }else{stop('Please select a valid output - `source` or `cdm`')}
   
-  fot <- fot_check(tblx = scv_process %>% ungroup(),
-                   target_col = output,
-                   facet_var = !!syms(facet))
+  fot <- fot_check(tblx = process_output %>% ungroup() %>%
+                     mutate(start_date = time_start),
+                   target_col = prop,
+                   facet_var = facet)
   
   fot2 <- check_fot_all_dist(fot_check_output = fot$fot_heuristic)
   
   mad <- produce_multisite_mad_scv(multisite_tbl = fot2,
-                                   code_type = code_type,
+                                   code_type = output,
                                    mad_dev = mad_dev)
   
   r <- ggplot(mad, aes(x=site, y=as.character(!!sym(y_col)), fill=grp_outlier_prop)) +
     geom_tile() +
     facet_wrap((facet)) +
+    scale_fill_viridis_c(option = 'turbo') +
     theme_classic() +
     coord_flip() +
     labs(title = 'Codes with Representative Proportions of Mappings +/- 2 MAD away from Median',
@@ -240,17 +187,18 @@ scv_ms_anom_at <- function(scv_process,
 #' using the CHOP-developed package called `rocqi` 
 #' 
 
-scv_ss_anom_at <- function(scv_process,
+scv_ss_anom_at <- function(process_output,
                            output,
                            facet){
   
   if(output == 'source'){
     col <- 'source_concept_id'
-  }else{(col <- 'concept_id')}
+  }else if(output == 'cdm'){
+    col <- 'concept_id'}
   
   facet <- facet %>% append(col)
   
-  n_mappings_yr <- scv_process %>%
+  n_mappings_yr <- process_output %>%
     group_by(!!!syms(facet), start_date) %>%
     summarise(n_mappings = n())
   
