f9cc5986af029f4327bf1fa70f083987de89b388
wieandk <wieandk@chop.edu>
2024-05-02 17:11:06 -0400

    testing output for attrition

--- code/cohort_ca.R
+++ code/cohort_ca.R
@@ -1,29 +1,242 @@
 
 
-compute_attrition_diff <- function(attrition_tbl){
+#' Compute difference between attrition steps
+#'
+#' @param attrition_tbl CSV file with attrition information - should include the following columns:
+#'                      
+#'                      num_pts | step_number | attrition_step | qry_site
+#' @param start_step_num integer indicating the number of the "start" step against which other steps should
+#'                       be compared
+#'
+#' @return the attrition information plus columns that describe the patient drop & percent difference between
+#'         each step and between each step and step 0
+#'
+
+compute_attrition_diff <- function(attrition_tbl,
+                                   start_step_num = 0){
   
-  if(! site %in% colnames(attrition_tbl)){attrition_tbl$site <- 'attrition_site'}
+  if(! 'qry_site' %in% colnames(attrition_tbl)){attrition_tbl$site <- 'attrition_site'}
   
   pct_prior <- attrition_tbl %>%
-    group_by(site) %>%
-    arrange(stepnum) %>%
+    filter(step_number >= start_step_num) %>%
+    group_by(qry_site) %>%
+    arrange(step_number) %>%
     mutate(
-      percent_prior_step = 100 * num_pats / lag(num_pats),
-      drop_prior_step = lag(num_pats) - num_pats,
-      drop_as_percent_prior = 100 * (lag(num_pats) - num_pats) / lag(num_pats)
+      percent_retained_prior = 100 * num_pts / lag(num_pts),
+      ct_diff_prior = lag(num_pts) - num_pts,
+      percent_diff_prior = 100 * (lag(num_pts) - num_pts) / lag(num_pts)
     ) %>%
     ungroup()
   
   step0_cts <- attrition_tbl %>%
-    filter(stepnum == 0) %>%
-    rename('step0_pts' = num_pats)
+    filter(step_number == start_step_num) %>%
+    rename('step0_pts' = num_pts) %>%
+    select(qry_site, step0_pts)
   
   pct_step0 <- attrition_tbl %>%
-    left_join(step0_cts, by = "site") %>%
-    mutate(percent_step0 = 100 * num_pats / step0_pts)
+    left_join(step0_cts) %>%
+    mutate(percent_retained_start = 100 * num_pts / step0_pts) %>%
+    select(-step0_pts)
   
   final_attrition <- attrition_tbl %>%
+    filter(step_number >= start_step_num) %>%
     left_join(pct_prior) %>%
-    left_join(pct_step0)
+    left_join(pct_step0) %>%
+    arrange(step_number)
+  
+  return(final_attrition)
+}
+
+
+combine_attritions <- function(site_list = c('seattle', 'colorado', 'chop'),
+                               file_directory = paste0(base_dir, '/results/')){
+  
+  attrition_list <- list()
+  
+  for(i in 1:length(site_list)){
+    
+    file_name <- paste0(site_list[i], '_diabetes_attrition.csv')
+    
+    file <- read_csv(file = paste0(file_directory, file_name))
+    
+    
+    attrition_list[[i]] <- file
+    
+  }
+  
+  combined_attrition <- reduce(.x = attrition_list,
+                               .f = dplyr::union)
+  
+  return(combined_attrition)
+  
+}
+
+
+#' Cohort Attrition Single Site Exploratory No Time
+#'
+#' @param process_output output from compute_attrition_diff
+#' @param log_scale logical to determine whether a log transform should be applied
+#'                  to the y axis
+#' @param output the column that should be used as the y-axis:
+#'               
+#'               options are: num_pts, percent_retained_prior, percent_retained_start
+#'
+#' @return a line graph with the output value for each step and an accompanying 
+#'         table with the full descriptions of each step
+#' 
+ca_ss_exp_nt <- function(process_output,
+                         log_scale = FALSE,
+                         output = 'num_pts'){
+  
+  cli::cli_div(theme = list(span.code = list(color = 'blue')))
+  
+  if(output == 'num_pts'){
+    title = 'Patient Count'
+  }else if(output == 'percent_retained_start'){
+    title = 'Percent Retained from Start'
+  }else if(output == 'percent_retained_prior'){
+    title = 'Percent Retained from Prior Step'
+  }else{cli::cli_abort("Please select a valid output option: {.code num_pts} or {.code percent_step0}")}
+  
+  min_step <- process_output %>% filter(step_number == min(step_number)) %>% pull(step_number)
+  max_step <- process_output %>% filter(step_number == max(step_number)) %>% pull(step_number)
+  
+  if(log_scale){
+    
+    grph <- ggplot(process_output %>% mutate(text = paste0('Step: ', attrition_step,
+                                                           '\nCount: ', num_pts,
+                                                           '\n % Retained: ', percent_retained_start)), 
+                   aes(y = !!sym(output), x = step_number)) +
+      geom_line(color = 'gray') +
+      geom_point_interactive(aes(color = as.character(step_number), tooltip = text), show.legend = FALSE) +
+      scale_y_continuous(transform = 'log2') +
+      scale_x_continuous(breaks = seq(min_step, max_step, 1)) +
+      labs(y = paste0(title, ' (Log)'),
+           x = 'Step',
+           title = paste0(title, ' per Attrition Step')) +
+      theme_minimal() +
+      scale_color_ssdqa()
+    
+    grph_int <- girafe(ggobj = grph)
+    
+  }else{
+    grph <- ggplot(process_output %>% mutate(text = paste0('Step: ', attrition_step,
+                                                           '\nCount: ', num_pts,
+                                                           '\n % Retained: ', round(percent_retained_start, 2), '%')), 
+                   aes(y = !!sym(output), x = step_number)) +
+      geom_line(color = 'gray') +
+      geom_point_interactive(aes(color = as.character(step_number), tooltip = text), show.legend = FALSE) +
+      scale_x_continuous(breaks = seq(min_step, max_step, 1)) +
+      scale_y_continuous(labels = scales::comma) +
+      labs(y = title,
+           x = 'Step',
+           title = paste0(title, ' per Attrition Step')) +
+      theme_minimal() +
+      scale_color_ssdqa()
+    
+    grph_int <- girafe(ggobj = grph)
+  }
+  
+  tbl <- process_output %>%
+    distinct(step_number, attrition_step) %>%
+    gt() %>%
+    cols_label('step_number' = 'Step Number',
+               'attrition_step' = 'Description') %>%
+    opt_stylize(style = 2) %>%
+    #opt_interactive() %>%
+    tab_header('Attrition Step Reference')
+  
+  output <- list(grph_int,
+                 tbl)
+  
+  
+  return(output)
+  
+}
+
+
+
+ca_ms_exp_nt <- function(process_output,
+                         log_scale = FALSE,
+                         output = 'num_pts'){
+  
+  cli::cli_div(theme = list(span.code = list(color = 'blue')))
+  
+  if(output == 'num_pts'){
+    title = 'Patient Count'
+  }else if(output == 'percent_retained_start'){
+    title = 'Percent Retained from Start'
+  }else if(output == 'percent_retained_prior'){
+    title = 'Percent Retained from Prior Step'
+  }else{cli::cli_abort("Please select a valid output option: {.code num_pts} or {.code percent_step0}")}
+  
+  min_step <- process_output %>% filter(step_number == min(step_number)) %>% pull(step_number) %>% unique()
+  max_step <- process_output %>% filter(step_number == max(step_number)) %>% pull(step_number) %>% unique()
+  
+  
+  allsite_med <- process_output %>%
+    group_by(step_number, attrition_step) %>%
+    summarise(allsite_median = median(!!sym(output)),
+              qry_site = 'All Site Median',
+              text = paste0('Site: ', qry_site,
+                            '\nMedian Value: ', allsite_median)) %>%
+    rename(!!output := allsite_median)
+  
+  if(log_scale){
+    
+    grph <- ggplot(process_output %>% mutate(text = paste0('Site: ', qry_site,
+                                                           '\nStep: ', attrition_step,
+                                                           '\nCount: ', num_pts,
+                                                           '\n % Retained Start: ', percent_retained_start)), 
+                   aes(y = !!sym(output), x = step_number, color = qry_site, group = qry_site)) +
+      geom_line() +
+      geom_line(data = allsite_med, linewidth = 1.1) +
+      geom_point_interactive(aes(tooltip = text), show.legend = FALSE) +
+      scale_y_continuous(transform = 'log2') +
+      scale_x_continuous(breaks = seq(min_step, max_step, 1)) +
+      labs(y = paste0(title, ' (Log)'),
+           x = 'Step',
+           color = 'Site',
+           title = paste0(title, ' per Attrition Step')) +
+      theme_minimal() +
+      scale_color_ssdqa()
+    
+    grph_int <- girafe(ggobj = grph)
+    
+  }else{
+    grph <- ggplot(process_output %>% mutate(text = paste0('Site: ', qry_site,
+                                                           '\nStep: ', attrition_step,
+                                                           '\nCount: ', num_pts,
+                                                           '\n % Retained Start: ', percent_retained_start)), 
+                   aes(y = !!sym(output), x = step_number, color = qry_site, group = qry_site)) +
+      geom_line() +
+      geom_line(data = allsite_med, linewidth = 1.1) +
+      geom_point_interactive(aes(tooltip = text), show.legend = FALSE) +
+      scale_x_continuous(breaks = seq(min_step, max_step, 1)) +
+      scale_y_continuous(labels = scales::comma) +
+      labs(y = title,
+           x = 'Step',
+           color = 'Site',
+           title = paste0(title, ' per Attrition Step')) +
+      theme_minimal() +
+      scale_color_ssdqa()
+    
+    grph_int <- girafe(ggobj = grph)
+  }
+  
+  tbl <- process_output %>%
+    distinct(step_number, attrition_step) %>%
+    gt() %>%
+    cols_label('step_number' = 'Step Number',
+               'attrition_step' = 'Description') %>%
+    opt_stylize(style = 2) %>%
+    #opt_interactive() %>%
+    tab_header('Attrition Step Reference')
+  
+  output <- list(grph_int,
+                 tbl)
+  
+  
+  return(output)
   
 }
--- code/driver_ca.R
+++ code/driver_ca.R
@@ -1,7 +1,7 @@
 .run <- function(){
   
   #' Get site name
-  config('qry_site', 'test')
+  config('qry_site', 'chop')
   
   #' Step 0: All patients with a visit between 2009-01-01 to 2023-12-31.
   
@@ -9,7 +9,7 @@
     filter(admit_date >= '2009-01-01' & admit_date <= '2023-12-31')
   
   init_sum(num_pts = distinct_ct(step0, id_col = 'patid'),
-           stepnum = 0,
+           step_number = 0,
            attrition_step = 'All patients with a visit between 2009-01-01 to 2023-12-31')
   
   #' Step 1: Patients with at least 5 outpatient visits between 2009-01-01 to 2023-12-31
@@ -18,10 +18,10 @@
     filter(enc_type %in% c('AV', 'TH')) %>%
     group_by(patid) %>%
     summarise(n_visit = n_distinct(encounterid)) %>%
-    filter(n_visit >= 5) %>% compute_new()
+    filter(n_visit >= 5)
   
   append_sum(num_pts = distinct_ct(step1, id_col = 'patid'),
-             stepnum = 1,
+             step_number = 1,
              attrition_step = 'Patients with at least 5 outpatient visits between 2009-01-01 to 2023-12-31')
   
   #' Step 2: Patients with at least 5 outpatient visits between 2009-01-01 to 2023-12-31, 
@@ -34,36 +34,110 @@
     mutate(min_visit = min(admit_date),
            max_visit = max(admit_date),
            visit_diff = (max_visit - min_visit) / 365.25) %>%
-    filter(visit_diff >= 2)
+    filter(visit_diff >= 2) %>%
+    ungroup() %>%
+    distinct(patid) %>% compute_new()
   
   append_sum(num_pts = distinct_ct(step2, id_col = 'patid'),
-             stepnum = 2,
-             attrition_step = 'Patients with at least 5 outpatient visits between 2009-01-01 to 2023-12-31, 
-             where time between first and last visit is at least two years')
+             step_number = 2,
+             attrition_step = 'Patients with at least 5 outpatient visits between 2009-01-01 to 2023-12-31, where time between first and last visit is at least two years')
   
   #' Step 3: Patients diagnosed with any Type II diabetes code.
   
+  step3 <- cdm_tbl('diagnosis') %>%
+    inner_join(step2) %>%
+    inner_join(load_codeset('t2d', 'cicc'), by = c('dx' = 'concept_code')) %>%
+    left_join(cdm_tbl('demographic') %>% select(patid, birth_date)) %>%
+    mutate(age_at_dx = (dx_date - birth_date) / 365.25) %>% compute_new()
+  
+  append_sum(num_pts = distinct_ct(step3, id_col = 'patid'),
+             step_number = 3,
+             attrition_step = 'Patients diagnosed with any Type II diabetes code')
+  
   #' Step 4: Patients at least 8 years old at time of first Type II diabetes code
+  
+  step4 <- step3 %>%
+    group_by(patid) %>%
+    filter(dx_date == min(dx_date),
+           age_at_dx >= 8) %>% ungroup()
+  
+  append_sum(num_pts = distinct_ct(step4, id_col = 'patid'),
+             step_number = 4,
+             attrition_step = 'Patients at least 8 years old at time of first Type II diabetes code')
   
   #' Step 5: Patients with at least two Type II diabetes codes.
   
+  step5 <- step3 %>% 
+    inner_join(step4 %>% select(patid)) %>%
+    select(diagnosisid, encounterid, patid, dx, dx_date) %>%
+    distinct() %>%
+    group_by(patid) %>%
+    summarise(n_dx = n()) %>%
+    filter(n_dx >= 2)
+  
+  append_sum(num_pts = distinct_ct(step5, id_col = 'patid'),
+             step_number = 5,
+             attrition_step = 'Patients with at least two Type II diabetes codes')
+  
   #' Step 6: Patients with an Hba1c lab in the record.
+  
+  step6 <- cdm_tbl('lab_result_cm') %>%
+    inner_join(select(step5, patid)) %>%
+    inner_join(load_codeset('hba1c', 'cicc'), by = c('lab_loinc' = 'concept_code')) %>% compute_new()
+  
+  append_sum(num_pts = distinct_ct(step6, id_col = 'patid'),
+             step_number = 6,
+             attrition_step = 'Patients with an Hba1c lab in the record')
   
   #' Step 7: Patients with an Hba1c lab with valid results.
   
+  step7 <- step6 %>%
+    filter(!is.na(result_num) & ! (result_num < 0 & result_num > 100))
+  
+  append_sum(num_pts = distinct_ct(step7, id_col = 'patid'),
+             step_number = 7,
+             attrition_step = 'Patients with an Hba1c lab with valid results')
+  
   #' Step 8: Patients with an Hba1c lab >= 6.5%
+  
+  step8 <- step7 %>%
+    filter(result_num >= 6.5)
+  
+  primary_cohort <- step8 %>% distinct(patid)
+  
+  append_sum(num_pts = distinct_ct(step8, id_col = 'patid'),
+             step_number = 8,
+             attrition_step = 'Patients with an Hba1c lab >= 6.5%')
   
   ## END PRIMARY COHORT ##
   
   #' Step 9: Patients with at least two Hba1c labs >= 6.5%
   
+  step9 <- step8 %>%
+    select(patid, encounterid, lab_result_cm_id, lab_loinc, result_num) %>%
+    distinct() %>%
+    group_by(patid) %>%
+    summarise(n_lab = n()) %>%
+    filter(n_lab >= 2)
+  
+  append_sum(num_pts = distinct_ct(step9, id_col = 'patid'),
+             step_number = 9,
+             attrition_step = 'Patients with at least two Hba1c labs >= 6.5%')
+  
   #' Step 10: Patients with an outpatient prescription for antidiabetic drug
   
+  step10 <- cdm_tbl('prescribing') %>%
+    inner_join(select(step9, patid)) %>%
+    inner_join(select(cdm_tbl('encounter'), encounterid, enc_type)) %>%
+    filter(enc_type %in% c('AV', 'TH')) %>%
+    inner_join(load_codeset('metformin'), by = c('rxnorm_cui' = 'concept_code'))
   
+  append_sum(num_pts = distinct_ct(step10, id_col = 'patid'),
+             step_number = 10,
+             attrition_step = 'Patients with an outpatient prescription for antidiabetic drug')
   
-  
-  
-  
+  output_sum(file = TRUE,
+             name = paste0(config('qry_site'), '_diabetes_attrition'))
   
   
 }
